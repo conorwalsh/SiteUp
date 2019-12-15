b020fa1d75f0279af8e7140e5ac385fea198348b
Conor Walsh <conorwalsh98@gmail.com>
2019-12-15 00:13:16 +0000

    MySQL Error correction

--- check.php
+++ check.php
@@ -44,17 +44,17 @@
 	
 	include('db.php');
 	
-	$dbpass = mysql_result(mysql_query("SELECT dbpass FROM cctvup_settings WHERE id=1 LIMIT 1;"),0);
+	$dbpass = mysql_result(mysql_query("SELECT dbpass FROM siteup_settings WHERE id=1 LIMIT 1;"),0);
 
 	if($pass==$dbpass){
 
 		$message;
 		$response;
 		
-		$site = mysql_result(mysql_query("SELECT site FROM cctvup_settings WHERE id=1 LIMIT 1;"),0);
+		$site = mysql_result(mysql_query("SELECT site FROM siteup_settings WHERE id=1 LIMIT 1;"),0);
 		
-		define('SITE_NAME', mysql_result(mysql_query("SELECT regardsname FROM cctvup_settings WHERE id=1 LIMIT 1;"),0)); 
-		define('SITE_URL', mysql_result(mysql_query("SELECT site FROM cctvup_settings WHERE id=1 LIMIT 1;"),0)); 
+		define('SITE_NAME', mysql_result(mysql_query("SELECT regardsname FROM siteup_settings WHERE id=1 LIMIT 1;"),0)); 
+		define('SITE_URL', mysql_result(mysql_query("SELECT site FROM siteup_settings WHERE id=1 LIMIT 1;"),0)); 
 	
 		function email_template($message) {
 			return '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" ><style>table td p {margin-bottom:15px;}</style></head><body style="padding:0;margin:0;background: #f3f3f3;font-family:arial, \'helvetica neue\', helvetica, serif" ><table cellpadding="0" cellspacing="0" border="0" align="center" width="100%" style="padding: 0 0 35px 0; background:#f3f3f3;"><tr><td align="center" style="margin: 0; padding: 0;"><center><table border="0" cellpadding="0" cellspacing="0" width="600"><tr><th style="padding:10px 0 10px 5px;text-align:left;vertical-align:top;" ><a href="'.SITE_URL.'" target="_blank" >'.SITE_NAME.'</a></th></tr><tr><td style="border-radius:5px;background:#fff;border:1px solid #e1e1e1;font-size:12px;font-family:arial, helvetica, sans-serif;padding:20px;font-size:13px;line-height:22px;" >'.$message.'</td></tr><tr><td style="padding-top:10px;font-size:10px;color:#aaa;line-height:14px;font-family:arial, \'helvetica neue\', helvetica, serif" ><p class="meta">This email is being sent to you from '.SITE_NAME.'.<br />&copy; <a href="http://conorwalsh.net/" style="font-size:10px;color:#aaa;line-height:14px;font-family:arial, \'helvetica neue\', helvetica, serif;">ConorWalsh.Net</a> '.date('Y').'</p></td></tr></table></center></td></tr></table></body></html>';
